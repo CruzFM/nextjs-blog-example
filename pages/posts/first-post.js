b430094a208952fd149f86d77b7e95123f3460b1
Fer Cruz <cruzfernandomartin94@gmail.com>
2023-04-18 23:15:24 -0300

    Layout implemented. Css styles implemented.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,20 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost(){
     return (
         <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script 
+                src='https://connect.facebook.net/en_US/sdk.js'
+                strategy='lazyOnload'
+                onLoad={()=> console.log('script loaded correctly, window.FB has been populated.')}
+            /> 
             <h1>
                 Dale Fer, vos podes, lucha por todo lo que queres.
             </h1>
@@ -11,6 +23,7 @@ export default function FirstPost(){
                     Back to home
                 </Link>
             </h2>
+        </Layout>
         </>
     );
 }
